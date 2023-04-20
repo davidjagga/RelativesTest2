fe485ffe74ed826c68fe0d0fbe44b136a2c3a987
davidjagga <djjagga@gmail.com>
2023-04-20 15:05:35 -0400

    requirements fix

--- imageanalysistest.py
+++ imageanalysistest.py
@@ -1,11 +1,11 @@
 import cv2
-
 def grayscale2(mainfile, relativefile):
     img_gray=cv2.imread(mainfile, 0)
     img_gray2 = cv2.imread(relativefile, 0)
     cv2.imwrite(mainfile, img_gray)
     cv2.imwrite(relativefile, img_gray2)
     analysisDict = {
+        'score':'',
         'height': 10,
         'eyeLength': 20,
         'skinColor': 'Brown',
